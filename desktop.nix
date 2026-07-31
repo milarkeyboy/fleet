171a1a2301b8a9175ba34322e09d7115c28f064a
Mitchell Larkin <mitchlarkin12@gmail.com>
2026-07-31 21:04:09 +0930

    Add NVIDIA driver for desktop

--- desktop.nix
+++ desktop.nix
@@ -12,5 +12,18 @@
 
   networking.hostName = "desktop";
 
-  # TODO: confirm GPU vendor/driver.
+  # Enable NVIDIA driver.
+  # Note that the 'xserver' part is just the name of the settings to
+  # turn on the NVIDIA driver; it's required for both X11 and Wayland.
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia = {
+    # The GeForce GTX 1060 uses the driver branch supporting Pascal GPUs.
+    branch = "legacy_580";
+    # Proprietary.
+    open = false;
+
+    # Wayland requires modesetting:
+    # - https://wiki.nixos.org/wiki/NVIDIA#Wayland
+    modesetting.enable = true;
+  };
 }
