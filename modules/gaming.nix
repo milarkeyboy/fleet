1c58866b088a17f51dda4d71a001183be7e93a66
Mitchell Larkin <mitchlarkin12@gmail.com>
2026-08-18 20:53:59 +0930

    Add discord, quake engine, and Dolphin emulator

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -9,6 +9,17 @@
     ];
   };
 
+  environment.systemPackages = with pkgs; [
+    # Console emulator
+    dolphin-emu
+
+    # Quake 3, which is better than the engine on
+    # Steam. Requires copying the assets to ~/.q3a.
+    quake3e
+
+    discord
+  ];
+
   # Enable OpenGL, Vulkan, etc.
   hardware.graphics.enable = true;
   hardware.graphics.enable32Bit = true;
